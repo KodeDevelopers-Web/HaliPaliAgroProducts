c903f508b32d6e7e3cbb2fd40ef3834e857b332f
KodeDevelopers-Web <ceo@kodedevelopers.org>
2026-06-17 13:10:50 +0530

    Fix bugs

--- checkout.js
+++ checkout.js
@@ -208,7 +208,7 @@ const placeOrder = async (event) => {
     subtotal: subtotal,
     total: subtotal,
     createdAt: new Date().toISOString(),
-    orderStatus: "Pending"
+    orderStatus: "Pending",
   };
 
   const itemsText = orderData.items
